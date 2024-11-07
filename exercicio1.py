ff7116aab9c62f635dfb002ff70768e6f3af76d3
Beatriz <168593598+monossacaridea@users.noreply.github.com>
2024-11-07 07:24:10 -0300

    Exercicio1.py
    Crie uma classe base Forma com um método desenhar() que desenha uma forma qualquer utilizando a biblioteca Turte. Em seguida, crie subclasses Círculo e Quadrado que herdam de Forma e sobrescrevem desenhar() para desenhar as formas específicas.

--- exercicio1.py
+++ exercicio1.py
@@ -2,34 +2,42 @@
 
 import turtle
 
+# Classe base Forma
 class Forma:
     def desenhar():
-        # método para desenhar a forma. deve ser sobrescrito pelas subclasses.
         pass
 
+# Subclasse Circulo
 class Circulo(Forma):
-    def desenhar(self):
-        # desenha um círculo usando a biblioteca turtle
+    def desenhar(self): # desenha um circulo
+        turtle.color("purple")
+        turtle.fillcolor("purple")
+        turtle.begin_fill()
         turtle.circle(50) # 50 é o raio do círculo
+        turtle.end_fill()
 
+# Subclasse Quadrado
 class Quadrado(Forma):
-    def desenhar(self):
-        # desenha um quadrado usando a biblioteca turtle
+    def desenhar(self): # desenha um quadrado
+        turtle.color("pink")
+        turtle.fillcolor("pink")
+        turtle.begin_fill()
         for _ in range(4):  # um quadrado tem 4 lados
             turtle.forward(100)  # avança 100 pixels
             turtle.right(90)     # vira 90 graus para a direita
+        turtle.end_fill()
 
 # Criando e desenhando um círculo
 circulo = Circulo()
 turtle.penup()
-turtle.goto(-75, 0)  # Move para a direita
+turtle.goto(-150, 0)  # Move para a direita
 turtle.pendown()
 circulo.desenhar()
 
 # Criando e desenhando um quadrado
 quadrado = Quadrado()
 turtle.penup()
-turtle.goto(-75, 0)
+turtle.goto(-50, 100)
 turtle.pendown()
 quadrado.desenhar()
 
