b5584b62b3ae8902226551cd9acf152d224a95e9
Beatriz <168593598+monossacaridea@users.noreply.github.com>
2024-11-07 07:26:55 -0300

    Exercicio 2.py
    Crie classes Motor, Pneu, Veiculo, onde Veiculo herda tanto de Motor quanto de Pneu.
    Ambas as classes base (Motor e Pneu) devem ter um método chamado status() que retorna uma string.
    A classe Veiculo deve também ter um método status() que chama os métodos status() das classes base.
    Implemente a herança de modo que a classe Veiculo resolva o método status() de maneira correta.

--- Exercicio 2.py
+++ Exercicio 2.py
@@ -1,4 +1,4 @@
-# Crie classes Motor, Pneu, Veiculo, onde Veiculo herda tanto de Motor quanto de Pneu.
+# Exercicio 2: Crie classes Motor, Pneu, Veiculo, onde Veiculo herda tanto de Motor quanto de Pneu.
 # Ambas as classes base (Motor e Pneu) devem ter um método chamado status() que retorna uma string.
 # A classe Veiculo deve também ter um método status() que chama os métodos status() das classes base.
 # Implemente a herança de modo que a classe Veiculo resolva o método status() de maneira correta.
